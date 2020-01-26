a6f9bde1cc30923a09fd21c252b96c4fb7d994b1
Ryan Urbanski <ryanurbanski9@gmail.com>
2020-01-26 01:32:20 -0500

    An echo client and server were set up.  Run the server file first, then client.  Type quit to exit

---
+++ Echo/src/echo/EchoClient.java
@@ -0,0 +1,71 @@
+/* 
+Project: Lab 1 - Group
+Purpose Details: Client-Server echo application
+Course: IST 411
+Author: Ryan Urbanski, Albana Beqo, James Bristow II, Michael Pratt
+Date Developed: 1/25/2020
+Last Date Changed:1/26/2020
+Revision: 1
+*/
+
+package echo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ryan
+ */
+public class EchoClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Simple Echo Client");
+        
+        try {                                                               
+            System.out.println("Waiting for connection...");
+            InetAddress localAddress = InetAddress.getLocalHost();
+            
+            try(Socket clientSocket = new Socket(localAddress, 6000);
+                    PrintWriter out = new PrintWriter( 
+                            clientSocket.getOutputStream(),true);
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(
+                            clientSocket.getInputStream()))) {
+                
+                // User will now be prompted to enter text
+                System.out.println("Connected to Server");
+                Scanner scanner = new Scanner(System.in);
+                while (true) {
+                    System.out.println("Enter text: ");
+                    String inputLine = scanner.nextLine();
+                    if ("quit".equalsIgnoreCase(inputLine)) {
+                        break;
+                    }
+                    out.println(inputLine);
+                    String response = br.readLine();
+                    System.out.println("Server response: " + response);
+                }
+                
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    
+}
--- Echo/src/echo/EchoServer.java
+++ Echo/src/echo/EchoServer.java
@@ -1,23 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 
+Project: Lab 1 - Group
+Purpose Details: Client-Server echo application
+Course: IST 411
+Author: Ryan Urbanski, Albana Beqo, James Bristow II, Michael Pratt
+Date Developed: 1/25/2020
+Last Date Changed:1/26/2020
+Revision: 1
+*/
+
 package echo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
- * @author James Bristow <jmb1079@psu.edu>
+ * @author ryan
  */
-public class EchoServer
-{
+public class EchoServer {
+    
+    public static void main (String[] args) {
+        System.out.println("This is a simple echo server");
+        
+        // This sets up the serverSocket to receive requests from a client
+        // onto the dedicated 6000 port.  It is on pause untill it gets a request
+        try (ServerSocket serverSocket = new ServerSocket(6000)) {
+            System.out.println("Waitining for a connection from a client...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Successfully onnected to client.");
+            
+            
+            // Reply to the clients request
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()));
+                    PrintWriter out = new PrintWriter (
+                            clientSocket.getOutputStream(), true)) {
+                
+                    // Reads 1 line at a time from the client and print to console
+                    String inputLine;
+                    while ((inputLine = br.readLine()) != null) {
+                        System.out.println("Server: " + inputLine);
+                        out.println(inputLine);
+                    }
+                    
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {
-        System.out.println("Simple Echo Server");
     }
     
 }
